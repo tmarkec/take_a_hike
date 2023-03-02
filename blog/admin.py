1ac4de20f0c7f3f3cd729e2b1d3452780f962097
tmarkec <tmarkec@gmail.com>
2023-03-02 14:37:43 +0000

    add post and comment model, and create superuser

--- blog/admin.py
+++ blog/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from django_summernote.admin import SummernoteModelAdmin
 
 # Register your models here.
+# @admin.register(Post)
+# class PostAdmin(SummernoteModelAdmin):
+
+#     summernote_fields = ('content')
