4d7d7518b5ba2f42c1ff3c4ffc78cff20cfdc9e2
tmarkec <tmarkec@gmail.com>
2023-03-02 15:26:31 +0000

    add post and comment model, linked bootstrap and css

--- blog/admin.py
+++ blog/admin.py
@@ -1,8 +1,25 @@
 from django.contrib import admin
+from .models import Post, Comment
 from django_summernote.admin import SummernoteModelAdmin
 
-# Register your models here.
-# @admin.register(Post)
-# class PostAdmin(SummernoteModelAdmin):
 
-#     summernote_fields = ('content')
+# Register your models here.
+@admin.register(Post)
+class PostAdmin(SummernoteModelAdmin):
+
+    list_display = ('title', 'slug', 'status', 'created_on')
+    search_fields = ['title']
+    prepopulated_fields = {'slug': ('title',)}
+    summernote_fields = ('content')
+
+
+@admin.register(Comment)
+class CommentAdmin(admin.ModelAdmin):
+    list_display = ('name', 'body', 'post', 'created_on', 'approved')
+    list_filter = ('created_on', 'approved')
+    search_fields = ['name', 'email_address', 'body']
+    actions = ['comments']
+
+    def comments(self, request, queryset):
+        queryset.update(approved=True)
+        
