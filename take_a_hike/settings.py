51918cb2f3f4158b65bcc69c0648f2e6342e34b2
tmarkec <tmarkec@gmail.com>
2023-03-02 11:18:54 +0000

    bug fix allowed hosts path file in settings

--- take_a_hike/settings.py
+++ take_a_hike/settings.py
@@ -31,7 +31,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["take_a_hike.herokuapp.com", "localhost"]
+ALLOWED_HOSTS = ["blog-hike.herokuapp.com", "localhost"]
 
 
 # Application definition
