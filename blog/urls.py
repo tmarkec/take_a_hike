7e369ed4ab20f1a3330396f826e53028e4037014
tmarkec <tmarkec@gmail.com>
2023-03-06 15:58:10 +0000

    bug fix display post.html

--- blog/urls.py
+++ blog/urls.py
@@ -2,5 +2,6 @@ from . import views
 from django.urls import path
 
 urlpatterns = [
-    path('', views.index, name='index')
+    path('', views.IndexView.as_view(), name='index'),
+    path('post/', views.PostList.as_view(), name='post')
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,30 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from django.views import generic, View
+from .models import Post
 
 
 # Create your views here.
-def index(request):
-    return render(request, 'index.html')
+# def index(request):
+#     return render(request, 'index.html')
+
+
+class IndexView(View):
+    def get(self, request, *args, **kwargs):
+        return render(request, 'index.html')
+
+
+class PostList(generic.ListView):
+    model = Post
+    queryset = Post.objects.filter(status=1).order_by('-created_on')
+    template_name = 'post.html'
+    paginate_by = 8
+
+# class PostDetail(View):
+#     def get(self, request, slug, *args, **kwargs):
+#         queryset = Post.objects.filter(status=1)
+#         post = get_object_or_404(querryset, slug=slug)
+#         comments = post.comments.filter(approved=True).order_by('created_on')
+#         liked = False
+#         if post.likes.filter(id=self.request.user.id).exist():
+#             liked = True
+#         return render(request, template_name)
