a9079281f691fa45f9d9e00b8aa7340f0ea66e31
tmarkec <tmarkec@gmail.com>
2023-03-14 15:02:48 +0000

    bug no local css style on deployed version

--- blog/urls.py
+++ blog/urls.py
@@ -11,6 +11,6 @@ urlpatterns = [
     path('profile/<int:user_id>/', views.profile, name='profile'),
     path('accounts/password_reset/', auth_views.PasswordResetView.as_view(template_name='account/password_reset.html'), name="password_reset"),
     path('accounts/password_reset/done/', auth_views.PasswordResetDoneView.as_view(template_name='account/password_reset_done.html'), name="password_reset_done"),
-    path('accounts/password_reset/<uidb64>/<token>', auth_views.PasswordResetConfirmView.as_view(template_name='account/password_reset_confirm.html'), name="password_reset_confirm"),
+    path('accounts/reset/<uidb64>/<token>', auth_views.PasswordResetConfirmView.as_view(template_name='account/password_reset_confirm.html'), name="password_reset_confirm"),
     path('accounts/password_reset_complete/', auth_views.PasswordResetCompleteView.as_view(), name="password_reset_complete"),
 ]
