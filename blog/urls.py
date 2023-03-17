8c68196576546efe0b459ede802f5a42af3b2bfb
tmarkec <tmarkec@gmail.com>
2023-03-17 13:04:14 +0000

    change file path pssword confirm

--- blog/urls.py
+++ blog/urls.py
@@ -18,7 +18,7 @@ urlpatterns = [
          auth_views.PasswordResetDoneView.as_view(
             template_name='account/password_reset_done.html'),
          name="password_reset_done"),
-    path('account/password_reset_confirm/<uidb64>/<token>',
+    path('account/password_reset/<uidb64>/<token>',
          auth_views.PasswordResetConfirmView.as_view(
           template_name='account/password_reset_confirm.html'),
          name="password_reset_confirm"),
