db23edc8c75480b51f31076a4d704837e03a1b1b
tmarkec <tmarkec@gmail.com>
2023-03-17 12:34:53 +0000

    removed test h1 tag from index, change path for password

--- blog/urls.py
+++ blog/urls.py
@@ -7,22 +7,22 @@ urlpatterns = [
     path('', views.IndexView.as_view(), name='index'),
     path('post/', views.PostList.as_view(), name='post'),
     path('<slug:slug>/', views.PostDetail.as_view(), name='single_post'),
-    path('accounts/signup/', views.register, name='account_register'),
+    path('account/signup/', views.register, name='account_register'),
     path('profile/<int:user_id>/', views.profile, name='profile'),
     path('profile', views.profile, name='profile'),
-    path('accounts/password_reset/',
+    path('account/password_reset/',
          auth_views.PasswordResetView.as_view(
             template_name='account/password_reset.html'),
          name="password_reset"),
-    path('accounts/password_reset/done/',
+    path('account/password_reset/done/',
          auth_views.PasswordResetDoneView.as_view(
             template_name='account/password_reset_done.html'),
          name="password_reset_done"),
-    path('accounts/reset/<uidb64>/<token>',
+    path('account/reset/<uidb64>/<token>',
          auth_views.PasswordResetConfirmView.as_view(
           template_name='account/password_reset_confirm.html'),
          name="password_reset_confirm"),
-    path('accounts/password_reset_complete/',
+    path('account/password_reset_complete/',
          auth_views.PasswordResetCompleteView.as_view(
           template_name='account/password_reset_complete'),
          name="password_reset_complete"),
